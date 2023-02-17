f6349019997c46e45855219271143150a18724e2
asheerhameed <asheerhameed54321@gmail.com>
2023-02-17 23:09:36 +0530

    finished working with home page component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,70 @@
-import React from 'react'
-import './home.css'
-import  { side, card4, card3, card2, card1, play } from './imports'
+import React from "react";
+import "./home.css";
+import {
+  side,
+  tabletside,
+  mobileside,
+  card4,
+  card3,
+  card2,
+  card1,
+  play,
+} from "./imports";
 const Home = () => {
   return (
-    <div>
-      <div>
-        <img src={side} alt="side" />
+    <div className="home__container section__padding">
+      <picture>
+        {/* <source media="(min-width: 420px)" srcset={mobileside} style={{marginTop:"2rem"}} />
+        <source media="(min-width: 960px)" srcset={tabletside} /> */}
+      </picture>
+      <div className="home_left_side">
+        <div className="home_left_side__container">
+          <div className="home_left_side__p">
+            <p>Smart Payments</p>
+          </div>
+          <div className="home_left_side__head">
+            <h1>
+              The Next Generation Payment{" "}
+              <span style={{ color: "var(--primary-color" }}>Method</span>
+            </h1>
+            <p>
+              Yet preference connection unpleasant yet melancholy but end
+              appearance. And excellence partiality estimating terminated day
+              everything.
+            </p>
+          </div>
+          <div className="home_left_side__cta">
+            <button className="home_left_side__cta__get-started">
+              Get started
+            </button>
+            <button className="home_left_side__cta__watch-video">
+              Watch video
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className="home_right_side">
+        <div className="right_side_photos">
+          <div className="photo">
+            <img src={card1} alt="card1" />
+          </div>
+          <div className="photo">
+            <img src={card2} alt="card2" />
+          </div>
+        </div>
+
+        <div className="right_side_photos">
+          <div className="photo">
+            <img src={card3} alt="card3" />
+          </div>
+          <div className="photo">
+            <img src={card4} alt="card4" />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Home/imports.js
+++ src/components/Home/imports.js
@@ -1,8 +1,9 @@
-import play from '../../assets/play.png';
-import card1 from '../../assets/card1.png';
-import card2 from '../../assets/card2.png';
-import card3 from '../../assets/card3.png';
-import card4 from '../../assets/card4.png';
-import side from '../../assets/side.png';
-
-export { side, card4, card3, card2, card1, play };
+import play from "../../assets/play.png";
+import card1 from "../../assets/card1.png";
+import card2 from "../../assets/card2.png";
+import card3 from "../../assets/card3.png";
+import card4 from "../../assets/card4.png";
+import side from "../../assets/side.png";
+import mobileside from "../../assets/mobileside.png";
+import tabletside from "../../assets/tabletside.png";
+export { tabletside, mobileside, side, card4, card3, card2, card1, play };
